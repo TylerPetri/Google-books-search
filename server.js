4f5d29a1c07e61d77c0f1500a155f229ea568ecd
T__R__P__53 <tyler_1701@hotmail.com>
2021-03-31 23:55:26 -0400

    env

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('./client/build') );
 
+if(process.env.NODE_ENV==="production"){
+  app.use(express.static("client/build"))
+}
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/books", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
