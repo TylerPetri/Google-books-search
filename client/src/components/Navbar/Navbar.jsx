d1155cd287ec3f106c387a84964c1c93ae4e6542
T__R__P__53 <tyler_1701@hotmail.com>
2021-04-20 21:11:32 -0400

    navbar styling

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import { Link, Route } from 'react-router-dom';
 import './Navbar.css'
-import { AiOutlineSearch } from 'react-icons/ai'
+import { GiBookshelf } from 'react-icons/gi'
+import { ImSearch } from 'react-icons/im'
 import Search from '../Search/Search'
 import Library from '../Library/Library'
 import SignUp from '../Signup/Signup'
@@ -23,23 +24,28 @@ function Navbar(props) {
     return (
         <>
         <nav className="navbar flex-nowrap">
-            <h3 className="dev">
-                Google Books
+            <Link to="/" className="devLink">
+            <h3 className="dev1">
+                Google
             </h3>
+            <h3 className="dev2">
+                Books
+            </h3>
+            </Link>
             <div className="searchContainer">
                 <input 
                     type="text" 
-                    className="form-control mx-0 searchInput" 
+                    className="form-control mx-0 pb-2 searchInput" 
                     placeholder="Search"
                 ></input>
                 <Link to='/'>
                     <button type="submit" className="searchBtn">
-                        <AiOutlineSearch/>
+                        <ImSearch className="btnIconSearch" />
                     </button>
                 </Link>
             </div>
-            <div className="nav-items flex-nowrap">
-                <Link to='/saved' className="nav-link">My Library</Link>
+            <div className="navItemsCont">
+                <Link to='/saved' className="navLink"><GiBookshelf/></Link><span className="libraryPopUp">My Library</span>
                 <Login toggleNav={closeNav}/>
             </div>
         </nav>
