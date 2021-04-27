49847013efaffbcc0a118e86123164c1b97b22c7
T__R__P__53 <tyler_1701@hotmail.com>
2021-04-26 20:05:23 -0400

    env

--- server.js
+++ server.js
@@ -4,18 +4,20 @@ const path = require('path')
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const cors = require('cors')
 
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors())
 
 
 if(process.env.NODE_ENV==="production"){
   app.use(express.static("client/build"))
 }
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/books", {
+mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
